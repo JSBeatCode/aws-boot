336dae56f56a2104dc9d597f843b376181af4343
jsdno0 <csdnum0@gmail.com>
2022-01-26 20:39:59 +0900

    first commit

---
+++ src/main/java/com/work/awsboot/AwsBootApplication.java
@@ -0,0 +1,13 @@
+package com.work.awsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBootApplication.class, args);
+	}
+
+}
